0a8193cf04e155145ff403123245a49f08f1ba10
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-02-21 12:28:13 +0700

    fix: optimize images

---
+++ packages/blog/src/mdx-components.js
@@ -0,0 +1,20 @@
+import { useMDXComponents as getBlogMDXComponents } from 'nextra-theme-blog'
+import { ImageZoom } from './components/image-zoom.tsx'
+
+const blogComponents = getBlogMDXComponents({
+  h1: ({ children }) => <h1 className="headline">{children}</h1>,
+  DateFormatter: ({ date }) =>
+    `Last updated at ${date.toLocaleDateString('en', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })}`,
+  img: ImageZoom
+})
+
+export function useMDXComponents(components) {
+  return {
+    ...blogComponents,
+    ...components
+  }
+}
