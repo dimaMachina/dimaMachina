af68d379cae08e2c79efb289a5fb6d27c3ef24e7
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-02-24 12:21:25 +0700

    prettier

--- packages/blog/src/app/projects/graphql-eslint.tsx
+++ packages/blog/src/app/projects/graphql-eslint.tsx
@@ -1,15 +1,19 @@
 import { FC } from 'react'
 
-export const GraphQLESLintLogo: FC<{ className?: string }> = ({ className = '' }) => {
+export const GraphQLESLintLogo: FC<{ className?: string }> = ({
+  className = ''
+}) => {
   return (
     <div
       role="presentation"
-      className={["bg-current inline-flex items-center justify-center size-8 rounded text-xs font-medium", className].filter(Boolean).join(' ')}
+      className={[
+        'inline-flex size-8 items-center justify-center rounded bg-current text-xs font-medium',
+        className
+      ]
+        .filter(Boolean)
+        .join(' ')}
     >
-      <span
-        role="img"
-        className="text-white dark:text-black"
-      >
+      <span role="img" className="text-white dark:text-black">
         ESL
       </span>
     </div>
