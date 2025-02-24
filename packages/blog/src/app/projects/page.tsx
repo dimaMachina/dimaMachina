e1e7378677b3a3351f31950c69c300a2af95f5fa
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-02-24 10:14:37 +0700

    more

---
+++ packages/blog/src/app/projects/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import IndexPage from './index.mdx'
+
+const Page: FC = () => {
+  return (
+    <>
+      <h1 className="headline">Projects</h1>
+      <IndexPage />
+    </>
+  )
+}
+
+export default Page
